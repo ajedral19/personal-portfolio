32c0184fc6af8a8a78e6cc7d8d42899996028503
ajedral <ajedral1994@gmail.com>
2021-11-05 13:37:06 +0800

    reset pages/[slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,16 +1,16 @@
-import Layout from '../components/Layout/layout'
-import { getData } from '../lib/data'
+// import Layout from '../components/Layout/layout'
+// import { getData } from '../lib/data'
 
-export async function getStaticPaths() {
-  const paths = null
-  return { paths, fallback: false }
-}
+// export async function getStaticPaths() {
+//   const paths = null
+//   return { paths, fallback: false }
+// }
 
-export async function getStatitProps({ params }) {
-  const portfolio = await getData('json')
-  return { props: { portfolio } }
-}
+// export async function getStatitProps({ params }) {
+//   const portfolio = await getData('json')
+//   return { props: { portfolio } }
+// }
 
-export default function Sample({ sampleData }) {
-  return <Layout></Layout>
-}
+// export default function Sample({ sampleData }) {
+//   return <Layout></Layout>
+// }
