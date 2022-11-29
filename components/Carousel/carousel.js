9fdb90d2f8b23ff8255f84842338349bba5cb173
ajedral <ajedral1994@gmail.com>
2022-11-29 21:35:42 +0800

    carousel

--- components/Carousel/carousel.js
+++ components/Carousel/carousel.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import style from "./carousel.module.sass";
 import cn from "classnames";
 import Card from "../Card/Card";
+import { el } from "date-fns/locale";
 // import { slide } from '../../utils/utils'
 
 const Carousel = ({ items = [] }) => {
@@ -32,44 +33,60 @@ const Carousel = ({ items = [] }) => {
 			title: "",
 			description: "",
 		},
+		{
+			id: "f",
+			title: "",
+			description: "",
+		},
+		{
+			id: "g",
+			title: "",
+			description: "",
+		},
 	];
-	const [currentIndex, setCurrentIndex] = useState(0);
 
-	const cardsOnDisplay = (n, n_start) => {
-		if (n > cards.length || !(n_start in cards)) return;
-		let arr = [];
-		for (let i = 1; i <= n; i++) {
-			if (n_start > cards.length - 1) n_start = 0;
-			arr = [...arr, n_start];
-			n_start += 1;
-		}
+	const [elements, setElements] = useState([])
 
-		return arr;
-	};
+	const constructElements = () => {
+		const elements_array = cards.map((item, key) =>
+			<Fragment key={key}>
+				<p>{item.id}</p>
+				<div className={cn(style.carousel__platform__item)}>
+					<Card type="image" />
+				</div>
+			</Fragment>
+		)
+		setElements(elements_array)
+	}
 
-	const slide = (next = false) => {
-		let i = currentIndex;
-		i = !next ? (currentIndex <= 0 ? cards.length - 1 : currentIndex - 1) : currentIndex >= cards.length - 1 ? 0 : currentIndex + 1;
-		setCurrentIndex(i);
-	};
 
-	console.log(cardsOnDisplay(4, currentIndex));
+	useEffect(() => {
+		constructElements()
+	}, [])
+
+
+	const slide = () => {
+		let arr = elements
+		let a = arr.shift()
+		arr.push(a)
+		setElements([...arr])
+	}
 
 	return (
 		<Fragment>
 			<div className={cn(style.carousel)}>
 				<div className={cn(style.carousel__control)}></div>
 				<div className={cn(style.carousel__platform)}>
-					{cards?.map((item, key, arr) => (
-						<div key={key} className={cn(style.carousel__platform__item, { [[style]["active"]]: item.visible })}>
-							<Card type="image" />
-						</div>
-					))}
+					{
+						elements?.map((item, key, arr) => (
+							item
+						)
+						)}
 				</div>
 				<button onClick={() => slide()}>prev</button>
-				<button onClick={() => slide(true)}>next</button>
+				<button onClick={() => slide()}>next</button>
 			</div>
-		</Fragment>
+		</Fragment >
 	);
 };
 
