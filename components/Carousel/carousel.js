3f82e4e75b1ad50d7b9dad9d4fdd9b8cbbae142e
fdcaje <fdc.adamjohnedral@gmail.com>
2022-11-29 08:53:56 +0800

    carousel logic

--- components/Carousel/carousel.js
+++ components/Carousel/carousel.js
@@ -6,93 +6,68 @@ import Card from "../Card/Card";
 // import { slide } from '../../utils/utils'
 
 const Carousel = ({ items = [] }) => {
-	let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	let cards = [
+		{
+			id: "a",
+			title: "",
+			description: "",
+		},
+		{
+			id: "b",
+			title: "",
+			description: "",
+		},
+		{
+			id: "c",
+			title: "",
+			description: "",
+		},
+		{
+			id: "d",
+			title: "",
+			description: "",
+		},
+		{
+			id: "e",
+			title: "",
+			description: "",
+		},
+	];
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-	// function getThreeValues(n) {
-	//   if (!(n in arr)) return
-	//   let _arr = [];
-	//   if (n == 0)
-	//     _arr = [arr.length - 1, n, n + 1]
-	//   else if (n >= arr.length - 1)
-	//     _arr = [n - 1, n, 0]
-	//   else
-	//     _arr = [n - 1, n, n + 1]
-	//   console.log(_arr)
-	// }
-	const [count, setCount] = useState(0);
-	const [carts, setCarts] = useState([]);
-	// useEffect(() => {
-	// 	let count = 0;
-	// 	window.setInterval(() => {
-	// 		getThreeValues(count);
-	// 		count += 1;
-	// 		if (count > arr.length - 1) count = 0;
-	// 	}, 2000);
-	// }, []);
-
-	function getThreeValues(n) {
-		if (!(n in arr)) return;
-		let _arr = [];
-		if (n == 0) _arr = [arr.length - 1, n, n + 1];
-		else if (n >= arr.length - 1) _arr = [n - 1, n, 0];
-		else _arr = [n - 1, n, n + 1];
-
-		const els = _arr.map((i) => (
-			<div key={i} className={cn(style.carousel__platform__item, `${i}`)} data-r="b">
-				<Card type="image" />
-			</div>
-		));
-
-		setEls(els);
-	}
-
-	const generateCarts = (data, n_of_display, n_start = null) => {
-		if (!(n_of_display in data) || n_of_display <= 0) return;
-
-		let right_excess;
-		let left_excess;
-		let new_arr = [];
-		for (let i = 0; i < n_of_display; i++) {
-			left_excess = !(i >= data.length) && data[data.length - 1];
-			right_excess = n_of_display >= data.length - 1 ? data[0] : data[n_of_display];
-			new_arr = [...new_arr, data[i]];
+	const cardsOnDisplay = (n, n_start) => {
+		if (n > cards.length || !(n_start in cards)) return;
+		let arr = [];
+		for (let i = 1; i <= n; i++) {
+			if (n_start > cards.length - 1) n_start = 0;
+			arr = [...arr, n_start];
+			n_start += 1;
 		}
-		return [left_excess, ...new_arr, right_excess];
+
+		return arr;
 	};
 
-	// const data = generateCarts([1, 4, 56, 7, 4, 2, 2, 3, 99], 8);
-	const data = generateCarts([1, 4, 56, 7, 4, 2, 2, 3, 99], 1);
+	const slide = (next = false) => {
+		let i = currentIndex;
+		i = !next ? (currentIndex <= 0 ? cards.length - 1 : currentIndex - 1) : currentIndex >= cards.length - 1 ? 0 : currentIndex + 1;
+		setCurrentIndex(i);
+	};
+
+	console.log(cardsOnDisplay(4, currentIndex));
 
 	return (
 		<Fragment>
 			<div className={cn(style.carousel)}>
 				<div className={cn(style.carousel__control)}></div>
 				<div className={cn(style.carousel__platform)}>
-					{data?.map((item, key) => (
-						<div id={key} key={key} className={cn(style.carousel__platform__item)}>
+					{cards?.map((item, key, arr) => (
+						<div key={key} className={cn(style.carousel__platform__item, { [[style]["active"]]: item.visible })}>
 							<Card type="image" />
 						</div>
 					))}
-					{/* <div className={cn(style.carousel__platform__item)}>
-						<Card type="image" />
-            </div>
-					<div className={cn(style.carousel__platform__item)}>
-          <Card type="image" />
-					</div>
-					<div className={cn(style.carousel__platform__item)}>
-						<Card type="image" />
-            </div>
-					<div className={cn(style.carousel__platform__item)}>
-          <Card type="image" />
-					</div>
-					<div className={cn(style.carousel__platform__item)}>
-						<Card type="image" />
-					</div>
-					<div className={cn(style.carousel__platform__item)}>
-          <Card type="image" />
-        </div> */}
 				</div>
-				<button>next</button>
+				<button onClick={() => slide()}>prev</button>
+				<button onClick={() => slide(true)}>next</button>
 			</div>
 		</Fragment>
 	);
