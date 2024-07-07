b053f1e55486672dd2d6d16cbd6e39cae95120d5
ajedral <ajedral1994@gmail.com>
2024-07-07 22:43:12 +0800

    major changes implemented (initial commit)

--- components/ImageThumbnail.js
+++ components/ImageThumbnail.js
@@ -1,37 +1,41 @@
-import { Fragment } from 'react'
-import Image from 'next/image'
-import cn from 'classnames'
-import test_img from '../assets/images/content/test_img.jpg'
+import { Fragment, useEffect, useState } from "react";
+import Image from "next/image";
+import cn from "classnames";
+import test_img from "../assets/images/content/test_img.jpg";
 
-const ImageThumbnail = ({
-  color,
-  data,
-  square = false,
-}) => {
-  const { title, description } = data || {}
-  return (
-    <Fragment>
-      <div className={cn('image-thumbnail', color, { square })}>
-        <div className="meta-data t-left">
-          {title && (
-            <span className='block meta-data-content'>
-              <h4 className="title size-normal medium t-cap">{title}</h4>
-              {/* <span className="block subject t-upper">{subject}</span> */}
-            </span>
-          )}
-        </div>
-        <div className="background">
-          <Image
-            layout='intrinsic'
-            objectFit='contain'
-            src={test_img}
-            alt="this is a test"
-          />
-        </div>
-      </div>
-    </Fragment>
-  )
-}
+const ImageThumbnail = ({ color, data, square = false }) => {
+	const [image, setImage] = useState([]);
+	const { title, description } = data || {};
 
+	useEffect(() => {
+		setImage(data?.display_image);
+	}, [data]);
 
-export default ImageThumbnail
+	return (
+		<Fragment>
+			<div className={cn("thumbnail-card", color, { square })}>
+				<div className="thumbnail-card__content-wrap t-left">
+					{title && (
+						<div className="block content">
+							<p>{image}</p>
+							<h4 className="title medium t-cap">{title}</h4>
+							{/* <p className="title size-normal">{data.meta_description}</p> */}
+							{/* <span className="block subject t-upper">{subject}</span> */}
+						</div>
+					)}
+				</div>
+				<div className="background">
+					{/* <Image
+						layout="responsive"
+						objectFit="contain"
+						className="testimg"
+						src={image}
+						alt="this is a test"
+					/> */}
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default ImageThumbnail;
--- utils/utils.js
+++ utils/utils.js
@@ -1,68 +1,60 @@
 export const collapse_ = (e, classes = []) => {
-  const elements = [...e.target.parentElement.children]
-  const res = elements.find((i) => i.classList.contains('content'))
-  if (classes) classes.forEach((c) => res.classList.toggle(c))
-  const collapseIcon = elements[0].children[0]
+	const elements = [...e.target.parentElement.children];
+	const res = elements.find((i) => i.classList.contains("content"));
+	if (classes) classes.forEach((c) => res.classList.toggle(c));
+	const collapseIcon = elements[0].children[0];
 
-  res.style.height = res.scrollHeight + 'px'
-  res.classList.toggle('collapse')
-  collapseIcon.classList.toggle('collapse')
+	res.style.height = res.scrollHeight + "px";
+	res.classList.toggle("collapse");
+	collapseIcon.classList.toggle("collapse");
 
-  if (!res.classList.contains('collapse')) {
-    res.addEventListener('transitionend', () =>
-      res.style.removeProperty('height'),
-    )
-  } else window.setTimeout(() => res.style.removeProperty('height'))
-}
+	if (!res.classList.contains("collapse")) {
+		res.addEventListener("transitionend", () => res.style.removeProperty("height"));
+	} else window.setTimeout(() => res.style.removeProperty("height"));
+};
 
 // icon must be element 0
 // content element must be grouped in a container with className content
 // collapse style accordion defualt to true
 export const collapse = (e, className = null, accordion = true) => {
-  const el = e.target.ownerDocument.activeElement
-  const collapseIcon = el.children[0] || null
-  const parent = el.parentElement.children
-  const contentElement =
-    [...parent].find((child) => child.classList.contains('content')) || null
+	const el = e.target.ownerDocument.activeElement;
+	const collapseIcon = el.children[0] || null;
+	const parent = el.parentElement.children;
+	const contentElement = [...parent].find((child) => child.classList.contains("content")) || null;
 
-  if (contentElement) {
-    if (accordion)
-      contentElement.style.height = contentElement.scrollHeight + 'px'
-    el.classList.toggle(className || 'collapse')
-    contentElement.classList.toggle(className || 'collapse')
-    collapseIcon && collapseIcon.classList.toggle(className || 'collapse')
+	if (contentElement) {
+		if (accordion) contentElement.style.height = contentElement.scrollHeight + "px";
+		el.classList.toggle(className || "collapse");
+		contentElement.classList.toggle(className || "collapse");
+		collapseIcon && collapseIcon.classList.toggle(className || "collapse");
 
-    if (accordion) {
-      if (!contentElement.classList.contains(className || 'collapse'))
-        contentElement.addEventListener('transitionend', () =>
-          contentElement.style.removeProperty('height'),
-        )
-      else
-        window.setTimeout(() => contentElement.style.removeProperty('height'))
-    }
-  }
-}
+		if (accordion) {
+			if (!contentElement.classList.contains(className || "collapse")) contentElement.addEventListener("transitionend", () => contentElement.style.removeProperty("height"));
+			else window.setTimeout(() => contentElement.style.removeProperty("height"));
+		}
+	}
+};
 
 export const navigateTo = (router, target, padding = 20) => {
-  if (typeof target == undefined || !target) return
-  if (window.location != '/') return router.push(`/#${target}`, '', { shallow: true })
-  window.scrollTo({
-    top: document.getElementById(target)?.offsetTop - padding,
-    left: 0,
-    behavior: 'smooth',
-  })
-}
+	if (typeof target == undefined || !target) return;
+	if (window.location != "/") return router.push(`/#${target}`, "", { shallow: true });
+	window.scrollTo({
+		top: document.getElementById(target)?.offsetTop - padding,
+		left: 0,
+		behavior: "smooth",
+	});
+};
 
 export const switchTheme = (state, setState) => {
-  setState({ ...state, theme_dark: !state.theme_dark })
-  // document.documentElement.classList.toggle('dark-mode')
-  // window.matchMedia('(prefers-color-scheme: dark)')
-}
+	setState({ ...state, theme_dark: !state.theme_dark });
+	// document.documentElement.classList.toggle('dark-mode')
+	// window.matchMedia('(prefers-color-scheme: dark)')
+};
 
 export const localStore = () => {
-  const t = window.caches.open
-  console.log(CacheStorage)
-}
+	const t = window.caches.open;
+	console.log(CacheStorage);
+};
 
 // export const collapse = (
 //   accordion = undefined,
@@ -84,34 +76,43 @@ export const localStore = () => {
 // }
 
 export const slide = (prev = false, next = false, container = undefined) => {
-  if (container) {
-    const scrollWidth = container.clientWidth
-    if (next) container.scrollBy(scrollWidth, 0)
-    if (prev) container.scrollBy(-scrollWidth, 0)
-  }
-}
+	if (container) {
+		const scrollWidth = container.clientWidth;
+		if (next) container.scrollBy(scrollWidth, 0);
+		if (prev) container.scrollBy(-scrollWidth, 0);
+	}
+};
 
 export const toggleElement = (e, el, className) => {
-  const element = document.querySelector('.' + el)
-  element.classList.toggle(className)
-  e.target.classList.toggle(className)
-}
+	const element = document.querySelector("." + el);
+	element.classList.toggle(className);
+	e.target.classList.toggle(className);
+};
 
-export const typewritter = (setWord, words, arr_index = 0, word = "", word_index = 0, timeout = undefined) => {
-  setWord(words[arr_index])
-  // clearTimeout(timeout)
-  // let curIndex = arr_index >= words.length ? 0 : arr_index
-  // const currentWord = words[arr_index]
-  // let w_i = word_index
-  // let typing = currentWord[w_i]
-  // setWord(typing)
-  // timeout = setTimeout(() => {
-  //   typewritter(setWord, words, curIndex, typing, w_i, timeout)
-  // }, 2000)
-}
+export const typewritter = (words, word_index, char_index, setter) => {
+	let word = [];
+	for (let i = 0; i < words[word_index].length; i++) {
+		word.push(words[word_index][i]);
+	}
+	setter(word.join(""));
+};
 
-export const onLoadEffect = (effect_type, duration, interval) => { }
+export const _typewritter = (setWord, words, arr_index = 0, word = "", word_index = 0, timeout = undefined) => {
+	setWord(words[arr_index]);
 
-export const swipe = () => { }
+	// clearTimeout(timeout)
+	// let curIndex = arr_index >= words.length ? 0 : arr_index
+	// const currentWord = words[arr_index]
+	// let w_i = word_index
+	// let typing = currentWord[w_i]
+	// setWord(typing)
+	// timeout = setTimeout(() => {
+	//   typewritter(setWord, words, curIndex, typing, w_i, timeout)
+	// }, 2000)
+};
 
-export const parallax = () => { }
+export const onLoadEffect = (effect_type, duration, interval) => {};
+
+export const swipe = () => {};
+
+export const parallax = () => {};
