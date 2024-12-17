38f25323cf0039b2fa599708b60da7f0924e9fc6
ajedral19 <ajedral1994@gmail.com>
2024-12-17 08:50:05 +0800

    infinite scroll

--- components/Carousel/carousel.jsx
+++ components/Carousel/carousel.jsx
@@ -1,19 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import cn from "classnames";
 import style from "./carousel.module.sass";
 
 const Carousel = ({ items = [] }) => {
-	return (
-		<Fragment>
-			{items.length && (
-				<ul className={cn(style.carousel)}>
-					{items.map((item) => (
-						<li key={item.name}>{item.icon}</li>
-					))}
-				</ul>
-			)}
-		</Fragment>
-	);
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        if (load) {
+            const scroller = document.querySelector(`.${style.infinite_scroll__scroller}`);
+            const items = Array.from(scroller.children);
+            items.forEach((item) => {
+                const clone = item.cloneNode(true);
+                clone.setAttribute("area-hidden", "true");
+                scroller.appendChild(clone);
+            });
+        }
+
+        return () => setLoad(true);
+    }, [load]);
+
+    return (
+        <Fragment>
+            {items.length && (
+                <div className={cn(style.infinite_scroll)}>
+                    <ul className={cn(style.infinite_scroll__scroller)}>
+                        {items.map((item) => (
+                            <li key={item.name}>{item.icon}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </Fragment>
+    );
 };
 
 export default Carousel;
--- pages/index.jsx
+++ pages/index.jsx
@@ -19,220 +19,206 @@ import { RiNextjsFill } from "react-icons/ri";
 import { DiNodejs, DiPhp } from "react-icons/di";
 
 export async function getStaticProps() {
-	const projectsData = await fetch_parsed_data("json");
-	const experiences = await fetch_parsed_data("markdown");
-	return { props: { projects: projectsData.data.projects, experiences } };
+    const projectsData = await fetch_parsed_data("json");
+    const experiences = await fetch_parsed_data("markdown");
+    return { props: { projects: projectsData.data.projects, experiences } };
 }
 
 const template = {
-	title: "",
-	desc: "",
+    title: "",
+    desc: "",
 };
 
+const size = 32;
+
 const carouselItems = [
-	{
-		name: "PHP",
-		icon: <DiPhp size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "NodeJS",
-		icon: <DiNodejs size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Typescript",
-		icon: <SiTypescript size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "React",
-		icon: <FaReact size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "NextJS",
-		icon: <RiNextjsFill size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Python",
-		icon: <FaPython size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Sass",
-		icon: <FaSass size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "git",
-		icon: <FaGitAlt size={32} title="lipsum" color="currentColor" />,
-	},
-	// {
-	// 	name: "Docker",
-	// 	icon: <FaDocker size={32} title="EC2, ECS, Lambda, S3, CloudWatch" color="currentColor" />,
-	// },
-	// {
-	// 	name: "AWS",
-	// 	icon: <FaAws size={32} title="EC2, ECS, Lambda, S3, CloudWatch" color="currentColor" />,
-	// },
-	{
-		name: "Adobe Photoshop",
-		icon: <SiAdobephotoshop size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Adobe Illustrator",
-		icon: <SiAdobeillustrator size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Adobe XD",
-		icon: <SiAdobexd size={32} title="lipsum" color="currentColor" />,
-	},
-	{
-		name: "Figma",
-		icon: <BiLogoFigma size={32} title="lipsum" color="currentColor" />,
-	},
+    {
+        name: "PHP",
+        icon: <DiPhp size={size} title="PHP" color="currentColor" />,
+    },
+    {
+        name: "NodeJS",
+        icon: <DiNodejs size={size} title="NodeJS" color="currentColor" />,
+    },
+    {
+        name: "Typescript",
+        icon: <SiTypescript size={size} title="Typescript" color="currentColor" />,
+    },
+    {
+        name: "React",
+        icon: <FaReact size={size} title="React" color="currentColor" />,
+    },
+    {
+        name: "NextJS",
+        icon: <RiNextjsFill size={size} title="NextJS" color="currentColor" />,
+    },
+    {
+        name: "Python",
+        icon: <FaPython size={size} title="Python" color="currentColor" />,
+    },
+    {
+        name: "Sass",
+        icon: <FaSass size={size} title="Sass" color="currentColor" />,
+    },
+    {
+        name: "git",
+        icon: <FaGitAlt size={size} title="git" color="currentColor" />,
+    },
+    // {
+    // 	name: "Docker",
+    // 	icon: <FaDocker size={size} title="EC2, ECS, Lambda, S3, CloudWatch" color="currentColor" />,
+    // },
+    // {
+    // 	name: "AWS",
+    // 	icon: <FaAws size={size} title="EC2, ECS, Lambda, S3, CloudWatch" color="currentColor" />,
+    // },
+    {
+        name: "Adobe Photoshop",
+        icon: <SiAdobephotoshop size={size} title="Adobe Photoshop" color="currentColor" />,
+    },
+    {
+        name: "Adobe Illustrator",
+        icon: <SiAdobeillustrator size={size} title="Adobe Illustrator" color="currentColor" />,
+    },
+    {
+        name: "Adobe XD",
+        icon: <SiAdobexd size={size} title="Adobe XD" color="currentColor" />,
+    },
+    {
+        name: "Figma",
+        icon: <BiLogoFigma size={size} title="Figma" color="currentColor" />,
+    },
 ];
 
 export default function Home({ projects, experiences }) {
-	// const [skills, setSkills] = useState(["code", "design", "any"]);
-	// const [devWorks, setDevWorks] = useState(template);
-	// const [designWorks] = useState(template); //setDesignWorks
-	const [state, setState] = useState({
-		promptModal: true,
-	});
+    // const [skills, setSkills] = useState(["code", "design", "any"]);
+    // const [devWorks, setDevWorks] = useState(template);
+    // const [designWorks] = useState(template); //setDesignWorks
+    const [state, setState] = useState({
+        promptModal: false,
+    });
 
-	const [word, setWord] = useState("");
+    const [word, setWord] = useState("");
 
-	const handleModalOnClick = () => setState({ ...state, promptModal: false });
+    const handleModalOnClick = () => setState({ ...state, promptModal: false });
 
-	// to do next
+    // to do next
 
-	useEffect(() => {
-		// typewritter(skills, 0, 0, setWord);
-		// changeWord(Math.floor(Math.random() * skillset.length))
-	}, [word]);
+    useEffect(() => {
+        // typewritter(skills, 0, 0, setWord);
+        // changeWord(Math.floor(Math.random() * skillset.length))
+    }, [word]);
 
-	useEffect(() => {
-		document.body.style.overflow = state.promptModal ? "hidden" : "unset";
-	}, [state.promptModal]);
+    useEffect(() => {
+        document.body.style.overflow = state.promptModal ? "hidden" : "unset";
+    }, [state.promptModal]);
 
-	return (
-		<Fragment>
-			{state.promptModal && <Modal onClick={handleModalOnClick} />}
-			<main>
-				<NavBar container />
-				<header>
-					<div className="container">
-						<h1 className={cn("title size-normal medium", style.intro)}>
-							<span className="block title bold size-large">AJ Edral</span>
-							Your regular normal neighboor
-						</h1>
-					</div>
-				</header>
-				<section className="container t-left pt-6 pb-6">
-					<div className="row center-items split-lg reverse-md reverse-lg">
-						<div className="col-lg-6 col-md-3 col-sm-4">
-							<Image
-								src="/assets/the_me.gif"
-								alt=""
-								width={540}
-								height={260}
-								layout="responsive"
-								objectFit="cover"
-								className="greyscale"
-							/>
-						</div>
-						<div className="col-lg-5 col-md-3 col-sm-4">
-							<h3 className="mb-2 title semibold">Hey Neighboor</h3>
-							<p>
-								Just a quick brief. I was a graphic designer, I specialized on creating logos and digital marketing
-								assets. I then worked as front-end developer and now I am as a somehow a Web Developer.
-							</p>
-						</div>
-					</div>
-					<div className="pt-6">
-						<Carousel items={carouselItems} />
-					</div>
-				</section>
+    return (
+        <Fragment>
+            {state.promptModal && <Modal onClick={handleModalOnClick} />}
+            <main>
+                <NavBar container />
+                <header>
+                    <div className="container">
+                        <h1 className={cn("title size-normal medium", style.intro)}>
+                            <span className="block title bold size-large">AJ Edral</span>
+                            Your regular normal neighboor
+                        </h1>
+                    </div>
+                </header>
+                <section className="container t-left pt-6 pb-6">
+                    <div className="row center-items split-lg reverse-md reverse-lg">
+                        <div className="col-lg-6 col-md-3 col-sm-4">
+                            <Image src="/assets/the_me.gif" alt="" width={540} height={260} layout="responsive" objectFit="cover" className="greyscale" />
+                        </div>
+                        <div className="col-lg-5 col-md-3 col-sm-4">
+                            <h3 className="mb-2 title semibold">Hey Neighboor</h3>
+                            <p>
+                                Just a quick brief. I was a graphic designer, I specialized on creating logos and digital marketing assets. I then worked as
+                                front-end developer and now I am as a somehow a Web Developer.
+                            </p>
+                        </div>
+                    </div>
+                </section>
 
-				<section className="pt-6 pb-6">
-					<div className="container">
-						<div className="row split-md relative">
-							<div className="col-lg-4 col-md-3 col-sm-4 lg-sticky">
-								<h4 className="title medium mb-4">Background and experiences</h4>
-								<p className="mb-2">
-									I am a Graphic Designer and Front-end (Web somehow) Developer. In 2017 I decided to do some coding and
-									set aside designing.
-								</p>
-								<p className="mb-2">
-									Besides, I love watching creepy YouTube videos/Horror Movies every night or dawn with lights turned
-									off, and that makes me feel better every time I felt tired of creating something.
-								</p>
-								<p>
-									Sounds nonsence, right?
-									<br />
-									But nevermind.
-								</p>
+                <section className="pt-6 pb-6">
+                    <div className="container">
+                        <Carousel items={carouselItems} />
+                    </div>
+                </section>
 
-								{/* <button className="btn">Download CV</button> */}
-							</div>
-							<div className="col-lg-5 col-md-3 col-sm-4">
-								<ol className="accordion pt-6">
-									{experiences.length
-										? experiences.map((experience, key) => (
-												<li key={key} className="mb-1 item">
-													<button
-														role="button"
-														className="btn clear full-width trigger t-cap"
-														onClick={(e) => collapse(e)}
-													>
-														<span className="collapse-icon mr-1 collapse">
-															<svg
-																xmlns="http://www.w3.org/2000/svg"
-																width="12"
-																height="12"
-																viewBox="0 0 12 12"
-																className="icon"
-															>
-																<g id="Group_114" data-name="Group 114" transform="translate(-799 -3136)">
-																	<rect
-																		className="rectA"
-																		data-name="Rectangle 220"
-																		width="12"
-																		height="2"
-																		transform="translate(799 3141)"
-																		fill="#eaeaea"
-																	/>
-																	<rect
-																		className="rectB"
-																		data-name="Rectangle 221"
-																		width="12"
-																		height="2"
-																		transform="translate(806 3136) rotate(90)"
-																		fill="#eaeaea"
-																	/>
-																</g>
-															</svg>
-														</span>
-														{experience.data.title}
-													</button>
-													<article className="content">
-														{experience.data.company && (
-															<h4 className="block t-cap title size-normal medium" style={{ lineHeight: 1.6 }}>
-																{experience.data.company}
-															</h4>
-														)}
-														<p className="mb-1 title size-normal t-cap">
-															{experience.data["year-started"] + " - " + experience.data["year-ended"]}
-														</p>
-														<p className="pb-1">{experience.content}</p>
-													</article>
-												</li>
-										  ))
-										: "sad, no experience yet"}
-								</ol>
-							</div>
-						</div>
-					</div>
-				</section>
+                <section className="pt-6 pb-6">
+                    <div className="container">
+                        <div className="row split-md relative">
+                            <div className="col-lg-4 col-md-3 col-sm-4 lg-sticky">
+                                <h4 className="title medium mb-4">Background and experiences</h4>
+                                <p className="mb-2">
+                                    I am a Graphic Designer and Front-end (Web somehow) Developer. In 2017 I decided to do some coding and set aside designing.
+                                </p>
+                                <p className="mb-2">
+                                    Besides, I love watching creepy YouTube videos/Horror Movies every night or dawn with lights turned off, and that makes me
+                                    feel better every time I felt tired of creating something.
+                                </p>
+                                <p>
+                                    Sounds nonsence, right?
+                                    <br />
+                                    But nevermind.
+                                </p>
 
-				<Footer container />
-			</main>
-		</Fragment>
-	);
+                                {/* <button className="btn">Download CV</button> */}
+                            </div>
+                            <div className="col-lg-5 col-md-3 col-sm-4">
+                                <ol className="accordion pt-6">
+                                    {experiences.length
+                                        ? experiences.map((experience, key) => (
+                                              <li key={key} className="mb-1 item">
+                                                  <button role="button" className="btn clear full-width trigger t-cap" onClick={(e) => collapse(e)}>
+                                                      <span className="collapse-icon mr-1 collapse">
+                                                          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" className="icon">
+                                                              <g id="Group_114" data-name="Group 114" transform="translate(-799 -3136)">
+                                                                  <rect
+                                                                      className="rectA"
+                                                                      data-name="Rectangle 220"
+                                                                      width="12"
+                                                                      height="2"
+                                                                      transform="translate(799 3141)"
+                                                                      fill="#eaeaea"
+                                                                  />
+                                                                  <rect
+                                                                      className="rectB"
+                                                                      data-name="Rectangle 221"
+                                                                      width="12"
+                                                                      height="2"
+                                                                      transform="translate(806 3136) rotate(90)"
+                                                                      fill="#eaeaea"
+                                                                  />
+                                                              </g>
+                                                          </svg>
+                                                      </span>
+                                                      {experience.data.title}
+                                                  </button>
+                                                  <article className="content">
+                                                      {experience.data.company && (
+                                                          <h4 className="block t-cap title size-normal medium" style={{ lineHeight: 1.6 }}>
+                                                              {experience.data.company}
+                                                          </h4>
+                                                      )}
+                                                      <p className="mb-1 title size-normal t-cap">
+                                                          {experience.data["year-started"] + " - " + experience.data["year-ended"]}
+                                                      </p>
+                                                      <p className="pb-1">{experience.content}</p>
+                                                  </article>
+                                              </li>
+                                          ))
+                                        : "sad, no experience yet"}
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <Footer container />
+            </main>
+        </Fragment>
+    );
 }
